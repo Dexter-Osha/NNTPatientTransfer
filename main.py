d297302e17ff96056dec8d3b69e96b2b81a3f731
Dexter-Osha <76421290+Dexter-Osha@users.noreply.github.com>
2025-02-14 14:57:06 -0500

    Updating Brains
    Patient's who have incorrect names (numbers mainly) will be put into a separate file and not added to copy/paste commands

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import openpyxl
 import os
 import keyboard
 import time
+import re
 
 # Main File of script, EDIT AT OWN RISK.
 if __name__ == '__main__':
@@ -11,9 +12,11 @@ if __name__ == '__main__':
     newFile = "C:\PatientTransfer\PatientEntryCommands.txt"
     errorFile= "C:\PatientTransfer\PatientEntryErrors.txt"
     df = pd.read_excel(file_path)
+    tempLength = 0
     # create a blank patient profile to be updated with each patient's corresponding info
     rawNNTPatient = {"Unique Index": "n/a", "/patid": "n/a", "/name": "n/a", "/surname": "n/a", "/dateb": "n/a",
                   "/SEX": "n/a"}
+    regex = '\d+'
 
 # Iterate through all rows in Excel files
     for row in df.itertuples():
@@ -32,10 +35,22 @@ if __name__ == '__main__':
             if key == "/dateb":
                 dateOfBirth_Timestamp = value
                 formattedDateOfBirth = (dateOfBirth_Timestamp.strftime('%d,%m,%Y'))
-        #Output patient entry command for each patient in newly created file.
-        with open(newFile, 'a') as file:
-            file.write("C:\\NNT\\NNTBridge.exe " + (str(NNTPatient).replace(":","").replace("'", "").replace(",","").strip("{}")) +
-                  " /dateb " + formattedDateOfBirth + "\n")
+            # Check the value stored for the name, if it is anything other than chars it will put the patient into an error file instead.
+            elif key == "/name":
+                stringValue = str(value)
+                temp = re.findall(regex,stringValue)
+                tempLength = len(temp)
+                if tempLength != 0:
+
+                    with open(errorFile, 'a') as file:
+                        file.write((str(NNTPatient).replace(":","").replace("'", "").replace(",","").strip("{}")) +
+                                   " /dateb " + formattedDateOfBirth + "\n")
+
+         #If patient is correct, put into patient entry command file.
+        if tempLength == 0:
+            with open(newFile, 'a') as file:
+                file.write("C:\\NNT\\NNTBridge.exe " + (str(NNTPatient).replace(":","").replace("'", "").replace(",","").strip("{}")) +
+                      " /dateb " + formattedDateOfBirth + "\n")
 
 
 
